70a8ede5a8d59bd63244bec919dc8c71c4e1b0b9
sottile27 <30367624+sottile27@users.noreply.github.com>
2017-12-07 10:27:30 +0300

    Update block.cpp

--- src/primitives/block.cpp
+++ src/primitives/block.cpp
@@ -8,38 +8,22 @@
 #include <hash.h>
 #include <tinyformat.h>
 #include <utilstrencodings.h>
-#include <chainparams.h>
-#include <consensus/params.h>
 #include <crypto/common.h>
-
-uint256 CBlockHeader::GetHash(const Consensus::Params& params) const
-{
-    int version;
-    if (nHeight >= (uint32_t)params.BTGHeight) {
-        version = PROTOCOL_VERSION;
-    } else {
-        version = PROTOCOL_VERSION | SERIALIZE_BLOCK_LEGACY;
-    }
-    CHashWriter writer(SER_GETHASH, version);
-    ::Serialize(writer, *this);
-    return writer.GetHash();
-}
 
 uint256 CBlockHeader::GetHash() const
 {
-    const Consensus::Params& consensusParams = Params().GetConsensus();
-    return GetHash(consensusParams);
+    return SerializeHash(*this);
 }
 
 std::string CBlock::ToString() const
 {
     std::stringstream s;
-    s << strprintf("CBlock(hash=%s, ver=0x%08x, hashPrevBlock=%s, hashMerkleRoot=%s, nHeight=%u, nTime=%u, nBits=%08x, nNonce=%u, vtx=%u)\n",
+    s << strprintf("CBlock(hash=%s, ver=0x%08x, hashPrevBlock=%s, hashMerkleRoot=%s, nTime=%u, nBits=%08x, nNonce=%u, vtx=%u)\n",
         GetHash().ToString(),
         nVersion,
         hashPrevBlock.ToString(),
         hashMerkleRoot.ToString(),
-        nHeight, nTime, nBits, nNonce,
+        nTime, nBits, nNonce,
         vtx.size());
     for (const auto& tx : vtx) {
         s << "  " << tx->ToString() << "\n";
